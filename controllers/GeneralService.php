baf632c07e0e3e52b73cc0cbecae2d525bb5d05a
Juan Carlos Ramírez <juancarlos.guerrero@palisis.com>
2024-06-27 09:54:30 +0200

    TOURCMS-10028 fix names and variables

--- controllers/GeneralService.php
+++ controllers/GeneralService.php
@@ -3,20 +3,19 @@ use onboarding\services\RedisService;
 
 class GeneralService
 {
-    static $cacheName;
+    const agentApiKeyRedisKey = 'agentApiKey';
 
     private $redis;
     private $expirationTime;
 
-    public function __construct($redis, $expirationTime)
+    public function __construct($redis)
     {
-        self::$cacheName = 'agentApiKey';
         $this->redis = $redis;
-        $this->expirationTime = $expirationTime;
+        $this->expirationTime = time() + 60*60*4;
     }
 
 
-    public function getXMLFromValidation($url, $paramsFromGet)
+    public function curlGetRequest($url, $paramsFromGet)
     {
 
         $url .= '?' . http_build_query($paramsFromGet);
@@ -44,7 +43,7 @@ class GeneralService
         return $xml;
     }
 
-    public function getXMLForAgent($url, $paramsForPost)
+    public function curlPostRequest($url, $paramsForPost)
     {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -73,27 +72,17 @@ class GeneralService
 
     public function cacheApiKeyAgent($apiKey)
     {
-        $this->redis->storeItemInRedis(self::$cacheName, $apiKey, RedisService::REDIS_TYPE_STRING);
-        $this->redis->expireAt(self::$cacheName, $this->expirationTime);
+        $this->redis->storeItemInRedis(self::agentApiKeyRedisKey, $apiKey, RedisService::REDIS_TYPE_STRING);
+        $this->redis->expireAt(self::agentApiKeyRedisKey, $this->expirationTime);
     }
 
     public function getApiKeyAgent()
     {
-        return $this->redis->getItemFromRedis(self::$cacheName, RedisService::REDIS_TYPE_STRING);
+        return $this->redis->getItemFromRedis(self::agentApiKeyRedisKey, RedisService::REDIS_TYPE_STRING);
     }
 
     public function checkApiKeyExists()
     {
-        return $this->redis->existKey(self::$cacheName);
-    }
-
-    public function setCacheName($cacheName)
-    {
-        self::$cacheName = $cacheName;
-    }
-    
-    public function getCacheName()
-    {
-        return self::$cacheName;
+        return $this->redis->existKey(self::agentApiKeyRedisKey);
     }
 }
